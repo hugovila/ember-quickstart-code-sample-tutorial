f2dfb96751c54916a3f59615bf2706d0c0e800ab
Hugo Vila <git@hugovila.nom.es>
2015-04-30 18:14:24 +0200

    Accept edits

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,23 +1,38 @@
 Todos.TodoController = Ember.ObjectController.extend({
-  isCompleted: function(key, value){
-    var model = this.get('model');
+    actions: {
+      editTodo: function () {
+        this.set('isEditing', true);
+      },
 
-    if (value === undefined) {
-      // property being used as a getter
-      return model.get('isCompleted');
-    } else {
-      // property bein used as a setter
-      model.set('isCompleted', value);
-      model.save();
-      return value;
-    }
-  }.property('model.isCompleted'),
+      acceptChanges: function () {
+        this.set('isEditing', false);
+        if (Ember.isEmpty(this.get('model.title'))) {
+          this.send('removeTodo');
+        } else {
+          this.get('model').save();
+        }
+      },
 
-  actions: {
-    editTodo: function() {
-      this.set('isEditing', true);
-    }
-  },
+      removeTodo: function () {
+        var todo = this.get('model');
+        todo.deleteRecord();
+        todo.save();
+      }
+    },
 
-  isEditing: false,
+    isEditing: false,
+
+    isCompleted: function (key, value){
+      var model = this.get('model');
+
+      if (value === undefined) {
+        // property being used as a getter
+        return model.get('isCompleted');
+      } else {
+        // property bein used as a setter
+        model.set('isCompleted', value);
+        model.save();
+        return value;
+      }
+    }.property('model.isCompleted')
 });
