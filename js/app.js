ee54a4095df4f1cfb9be2eff2381db5f773255e3
Hugo Vila <git@hugovila.nom.es>
2015-05-07 18:18:15 +0200

    Replace the fixture adapter with local adapter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
