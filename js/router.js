d3ee83b8cf9d88e57206b6de03cf3dad40088475
Hugo Vila <git@hugovila.nom.es>
2015-05-07 11:11:30 +0200

    Transition to show only complete todos

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes will go here later
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -12,6 +13,17 @@ Todos.TodosActiveRoute = Ember.Route.extend({
     });
   },
   renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.filter('todo', function (todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function (controller) {
     this.render('todos/index', {controller: controller});
   }
 });
